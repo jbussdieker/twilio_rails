b29bc84d170d8532b01ce4e3fc8cd5e5f4ea758a
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-14 13:39:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioRails::Application.config.secret_token = 'a42a1e84b79e05fbd0d781b4920acca823758efcb25611e228abd17b9583d07ee9c8cb3b29ced5262505b155d3972212dc227f50d39271c0fba4232cbaa9712d'
